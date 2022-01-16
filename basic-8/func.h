3b07d19985b459cd4c7da88f38b8d36b4d42e464
David Song <david990724@gmail.com>
2022-01-16 02:36:46 -0500

    Update on basic 8

--- basic-8/func.h
+++ basic-8/func.h
@@ -12,8 +12,21 @@ class PersonalInfo{
         int birthDay;
         bool isMarried;
     public:
+        PersonalInfo();
+        PersonalInfo(string n, int y, int m, int d, bool ism);
+        ~PersonalInfo();
+        
+        void setName(string n);
+        void setBirthday(int y, int m, int d);
+        void setMarried(bool ism);
 
-    
+        string getName();
+        int getbirthYear();
+        int getbirthMonth();
+        int getbirthDay();
+        bool getisMarried();
+
+        void print() const;
 };
 
 class AdditionalInfo: public PersonalInfo{
@@ -22,7 +35,19 @@ class AdditionalInfo: public PersonalInfo{
         string address;
         char sex;
     public:
+        AdditionalInfo();
+        AdditionalInfo(string ma, string ad, char sx);
+        ~AdditionalInfo();
 
+        void setmailAddr(string ma);
+        void setAddr(string ad);
+        void setSex(char sx);
+
+        string getmailAddr();
+        string getAddr();
+        char getSex();
+
+        void print() const;
     
 };
 
@@ -30,7 +55,15 @@ class FullContact: public AdditionalInfo, public PhoneInfo{
     private:
         string phoneNumber;
     public:
+        FullContact();
+        FullContact(string pn);
+        ~FullContact();
 
+        void setphoneNumber(string pn);
+
+        string getphoneNumber();
+
+        void print() const;
     
 };
 
@@ -39,7 +72,17 @@ class PhoneInfo {
         string model;
         string IMEI;
     public:
+        PhoneInfo();
+        PhoneInfo(string mo, string im);
+        ~PhoneInfo();
 
+        void setModel(string mo);
+        void setIMEI(string im);
+
+        string getModel();
+        string getIMEI();
+
+        void print() const;
 
 };
 
