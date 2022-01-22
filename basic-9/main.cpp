b0df6fdc0bdb4d18b44577e032ed3ff5210565d1
David Song <david990724@gmail.com>
2022-01-22 11:06:44 -0500

    Finished basic-9

--- basic-9/main.cpp
+++ basic-9/main.cpp
@@ -7,17 +7,52 @@ using namespace std;
 
 int main(){
     HashTable names;
-    names.insertElement("David Song");
-    names.insertElement("Ian Webster");
-    names.insertElement("Jun Ho Sung");
-    names.printTable();
-    names.insertElement("Yixin Tian");
-    names.printTable();
-    names.insertElement("Mymy Tran");
-    names.printTable();
-    names.deleteElement("David Song");
-    names.printTable();
-
+    int toDo;
+    string ip;
+    bool toEnd = false;
+    while(!toEnd){
+        ip = "";
+        cout << "Insert your operation. 1) Insert 2) Search 3) Delete 4) Print table" << endl;
+        cout << "Press any other key to EXIT... : ";
+        cin >> toDo;
+        cin.ignore();
+        cout << toDo << endl;
+        switch(toDo){
+            case 1:
+                cout << "Which name do you want to input?: ";
+                getline(cin,ip);
+                names.insertElement(ip);
+                break;
+            case 2:
+                cout << "Which name do you want to search?: ";
+                getline(cin,ip);
+                if(names.searchElement(ip)){
+                    cout << ip << " found" << endl;
+                }
+                else{
+                    cout << ip << " not found" << endl;
+                }
+                break;
+            case 3:
+                cout << "Which name do you want to delete?: ";
+                getline(cin,ip);
+                if(names.deleteElement(ip)){
+                    cout << "Successfully deleted " << ip << endl;
+                }
+                else{
+                    cout << "Unable to delete " << ip << endl;
+                }
+                break;
+            case 4:
+                cout << endl;
+                names.printTable();
+                cout << endl;
+                break;
+            default:
+                toEnd = true;
+                break;
+        }
+    }
     return 0;
 }
 
